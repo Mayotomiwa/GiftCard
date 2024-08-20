d1cf6a50446df45814406618a7581ab169600ee9
Oluseyi Immanuel <oluseyimayotomiwa@outlook.com>
2024-08-20 01:30:08 -0700

    chore: fixing smae image relaod

--- src/data/Data.tsx
+++ src/data/Data.tsx
@@ -1,29 +1,25 @@
-import axios from 'axios';
-import { UnsplashImage } from '../types/UnsplashImage';
+import axios from "axios";
+import { UnsplashImage } from "../types/UnsplashImage";
 
 const accessKey = import.meta.env.VITE_API_KEY;
 
-const cache: { [key: number]: UnsplashImage[] } = {};
-
-export const fetchImages = async (count: number = 4): Promise<UnsplashImage[]> => {
-    // Check if the data is in the cache
-    if (cache[count]) {
-        console.log('Returning cached data:', cache[count]); // Log cached data
-        return cache[count];
-    }
-
-    try {
-        await new Promise(resolve => setTimeout(resolve, 1000)); // 1 second delay
-        const response = await axios.get(`https://api.unsplash.com/photos/random?count=${count}`, {
-            headers: {
-                Authorization: `Client-ID ${accessKey}`,
-            },
-        });
-        console.log('API Response:', response.data); // Log the response data
-        cache[count] = response.data; // Cache the response data
-        return response.data;
-    } catch (error) {
-        console.error('Error fetching images:', error);
-        return [];
-    }
+export const fetchImages = async (
+  count: number = 4
+): Promise<UnsplashImage[]> => {
+  try {
+    await new Promise((resolve) => setTimeout(resolve, 1000)); // 1 second delay
+    const response = await axios.get(
+      `https://api.unsplash.com/photos/random?count=${count}`,
+      {
+        headers: {
+          Authorization: `Client-ID ${accessKey}`,
+        },
+      }
+    );
+    console.log("API Response:", response.data); // Log the response data
+    return response.data;
+  } catch (error) {
+    console.error("Error fetching images:", error);
+    return [];
+  }
 };
